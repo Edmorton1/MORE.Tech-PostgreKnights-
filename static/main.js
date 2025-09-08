3d50ad2ee651534689b9f9128000bf56c538b7a6
Edmorton1 <ghettoboy2106@gmail.com>
2025-09-08 10:49:06 +0500

    Обновил README.md

--- static/main.js
+++ static/main.js
@@ -2,3 +2,18 @@
 
 import "./form.js";
 import "./history.js";
+
+const textarea = document.getElementsByTagName("textarea");
+textarea[0].addEventListener("keydown", function (event) {
+  if (event.key === "Tab") {
+    event.preventDefault();
+
+    const start = this.selectionStart;
+    const end = this.selectionEnd;
+
+    this.value =
+      this.value.substring(0, start) + "\t" + this.value.substring(end);
+
+    this.selectionStart = this.selectionEnd = start + 1;
+  }
+});
